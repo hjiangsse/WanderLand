d1c984729c74a8aa793c9eba5491ff38144a5ab0
hjiangsse <hjiang@sse.com.cn>
2020-08-27 06:53:36 +0800

    commit

---
+++ Rust/rust_errors/target/debug/deps/rust_errors-a0f5427f9a68d3ef.d
@@ -0,0 +1,5 @@
+/home/pi/Playground/WanderLand/Rust/rust_errors/target/debug/deps/rust_errors-a0f5427f9a68d3ef: src/main.rs
+
+/home/pi/Playground/WanderLand/Rust/rust_errors/target/debug/deps/rust_errors-a0f5427f9a68d3ef.d: src/main.rs
+
+src/main.rs:
