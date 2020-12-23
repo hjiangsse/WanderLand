a986f972af87cd568c79bab04ac0b652ab54f103
hjiangsse <hjiang@sse.com.cn>
2020-12-23 10:08:02 +0800

    12/23/20 10:08:02

---
+++ hacking_llvm/clang_diagnositic/clang_diag.cpp
@@ -0,0 +1,13 @@
+extern "C"
+{
+    #include "clang-c/Index.h"
+}
+#include "llvm/Support/CommandLine.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "This is the end of the world" << endl;
+    return 0;
+}
