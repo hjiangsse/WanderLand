35e00d55ba1568f36fe9fc1db70b7eb893edf418
hjiangsse <redmagic039@gmail.com>
2021-01-25 22:04:05 +0800

    01/25/21 22:04:05

--- tao_of_cpp/essential_cpp/iter_class/test_tri.cpp
+++ tao_of_cpp/essential_cpp/iter_class/test_tri.cpp
@@ -6,5 +6,13 @@ using namespace std;
 
 int main(int argc, char **argv) {
   Triangular tri(10, 1);
+  Triangular::iterator it = tri.begin();
+  Triangular::iterator end_it = tri.end();
+
+  cout << tri.length() << endl;
+  while(it != end_it) {
+	cout << *it << endl;
+	++it;
+  }
   return 0;
 }
--- tao_of_cpp/essential_cpp/iter_class/tri.cpp
+++ tao_of_cpp/essential_cpp/iter_class/tri.cpp
@@ -1,4 +1,7 @@
 #include "tri.h"
+#include <algorithm>
+
+using namespace std;
 
 vector<int> Triangular::_elems;
 
@@ -20,12 +23,47 @@ bool Triangular::next(int &value) {
   return false;
 }
 
-void Triangular::gen_elements(int length) {
-  if(length > _length) {
-    _length = length;
+bool Triangular::is_elem(int value) {
+  if(!_elems.size() ||
+	 _elems[_elems.size() - 1] < value) {
+	gen_elems_to_value(value);
   }
 
-  for(int ix = 1; ix <= _length; ix++) {
-    _elems.push_back(ix*(ix +1)/2);
+  vector<int>::iterator found_it;
+  vector<int>::iterator end_it = _elems.end();
+
+  found_it = find(_elems.begin(), end_it, value);
+  return found_it != end_it;
+}
+
+void Triangular::gen_elems_to_value(int value) {
+  int ix = _elems.size();
+  if(!ix) {
+	_elems.push_back(1);
+	ix = 1;
+  }
+
+  while(_elems[ix - 1] < value && ix < _max_elems) {
+	++ix;
+	_elems.push_back(ix * (ix + 1) / 2);
+  }
+
+  if(ix == _max_elems) {
+	cerr << value << " -- exceeds max size of "
+	     << _max_elems << endl;
+  }
+}
+
+void Triangular::gen_elements(int length) {
+  if(length < 0 || length > _max_elems) {
+    cout << "wrong lenth" << endl;
+	return;
+  }
+
+  if(_elems.size() < length) {
+	int ix = _elems.size()?_elems.size() + 1: 1;
+	for(; ix <= length; ix++) {
+	  _elems.push_back(ix*(ix+1)/2);
+	}
   }
 }
